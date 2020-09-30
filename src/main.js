19bf9470e047d7ad485cf58046508fae8160d816
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2020-09-30 13:38:20 +0700

    17 - Update info by json

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import LoadScript from 'vue-plugin-load-script'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(LoadScript)
 
 new Vue({
   render: h => h(App),
